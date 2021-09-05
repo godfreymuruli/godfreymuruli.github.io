f2c737c8261a5ca21ca3d26daef8ca3ba03c0091
kimanicharles911 <kimanicharles911@gmail.com>
2021-09-06 00:53:26 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import ReactGA from "react-ga";
+import $ from "jquery";
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      resumeData: {},
+    };
+
+    ReactGA.initialize("UA-110570651-1");
+    ReactGA.pageview(window.location.pathname);
+  }
+
+  getResumeData() {
+    const load = document.getElementById("siteLoading");
+    $.ajax({
+      url: "/resumeData.json",
+      dataType: "json",
+      cache: false,
+      success: function (data) {
+        this.setState({ resumeData: data });
+        setTimeout(() => {
+          load.outerHTML = "";
+        }, 500);
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.log(err);
+        alert(err);
+      },
+    });
+  }
+
+  componentDidMount() {
+    this.getResumeData();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header data={this.state.resumeData.main} />
+        <About data={this.state.resumeData.main} />
+        <Contact data={this.state.resumeData.main} />
+        <Footer data={this.state.resumeData.main} />
+      </div>
+    );
+  }
+}
+
+export default App;
